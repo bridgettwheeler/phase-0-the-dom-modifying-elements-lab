85094e120f5e8070208a437ba416c923beffae36
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-06-14 15:01:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1'); 
+newHeader.id = "victory";
+document.body.appendChild(newHeader); 
+newHeader.innerHTML = "Bridgett is the champion";
